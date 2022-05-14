6288348160c210580581bbed84410532d2f4c037
Kevin Tuei <kevintuei89@gmail.com>
2022-05-14 18:29:16 +0300

    Javascript update to the Portfolio Project

---
+++ js/portfolio.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+    input("Enter your email for my weekly newsletter");
+	alert("Hello!");
+	alert("How are you?");
+    alert("Welcome to Stella Waithera Personal Portfolio");
+    
+}    
+
+function changeText() {
+	var span = document.getElementById("output");
+	var textBox = document.getElementById("textbox");
+	
+	 textbox.style.color = "red"; 
+		
+}
+
+function changeImage() {
+	var stellaimg = document.getElementById("stellaimg");
+	
+	 stellaimg.style.float = "left"; 
+		
+}
